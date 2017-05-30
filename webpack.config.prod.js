725ec47dc98be3da461bc31608c376f557831d08
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-05-31 00:38:18 +0300

    final commit. added bundle splitting.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,23 +1,31 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
     debug: true,
     devtool: 'source-map',
     noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
+    entry: {
+        vendor: path.resolve(__dirname, 'src/vendor'),
+        main: path.resolve(__dirname, 'src/index')
+    },
     target: 'web',
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: 'bundle.js'
+        filename: '[name].[chunkhash].js'
     },
     plugins: [
+        new ExtractTextPlugin('[name].[contenthash].css'),
+        new WebpackMd5Hash(),
         new webpack.optimize.UglifyJsPlugin(),
         new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
         new HtmlWebpackPlugin({
             template: 'src/index.html',
             minify: {
@@ -36,7 +44,7 @@ export default {
     module: {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-            { test: /\.css$/, loaders: ['style', 'css'] }
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
         ]
     }
 }
